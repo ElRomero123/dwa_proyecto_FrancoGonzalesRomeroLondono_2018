3209ece353f1a01a407cf227bcb80be4fc8ec1b4
Juan Pablo Romero Londono <juan.romerol@upb.edu.co>
2018-10-23 18:34:21 -0500

    Mostrar los aperitivos, platos y bebidas

--- final/js/reservaRestaurants.js
+++ final/js/reservaRestaurants.js
@@ -1,5 +1,6 @@
 ﻿var nombre, phone;
-var regresar;
+var back, menu;
+var botonId;
 
 window.onload = inicializar;
 
@@ -59,37 +60,92 @@ function initRestaurants()
 
 function initVariables()
 {
-    regresar = document.getElementById('regresar');
+    back = document.getElementById('back');
+    menu = document.getElementById('menu');
 }
 
 function initEventos()
 {
-    regresar.addEventListener('click', regresarMenu);
+    back.addEventListener('click', toBack);
+    menu.addEventListener('click', toMenu);
 }
 
-function cargarRestaurante(boton)
+function cargarRestaurante(btn)
 {
+    $('#listOptions').empty();
+
     $.ajax
     (
         {
-            url: '../api/food?idRestaurant=' + boton.id + '&type=' + 1,
+            url: '../api/food?idRestaurant=' + btn.id + '&type=' + 1,
             type: 'GET',
             contentType: "application/json;charset=utf-8",
             success:
             function (data)
             {
+                var elements = '';
+  
                 for (var i = 0; i < data.length; i++)
                 {
-                    alert(data[i][0] + " " + data[i][1]);
+                    elements += '<button id="' + data[i][0] + '" class="restaurants" onclick="cargarRestaurante(this)"> <div> <div>' + data[i][1] + '</div> <div>' + data[i][2] + '</div> <div>' + data[i][3] + '</div> <div>' + data[i][1] + '</div> </div> </button>';
                 }
+
+                $('#listOptions').append('<div id="aperitivo" class="banner">APERITIVOS</div>' + elements);
+            }
+        }
+    );
+
+    $.ajax
+    (
+        {
+            url: '../api/food?idRestaurant=' + btn.id + '&type=' + 2,
+            type: 'GET',
+            contentType: "application/json;charset=utf-8",
+            success:
+            function (data)
+            {
+                var elements = '';
+
+                for (var i = 0; i < data.length; i++)
+                {
+                    elements += '<button id="' + data[i][0] + '" class="restaurants" onclick="cargarRestaurante(this)"> <div> <div>' + data[i][1] + '</div> <div>' + data[i][2] + '</div> <div>' + data[i][3] + '</div> <div>' + data[i][1] + '</div> </div> </button>';
+                }
+
+                $('#listOptions').append('<div id="plato" class="banner">PLATOS</div>' + elements);
+            }
+        }
+    );
+
+    $.ajax
+    (
+        {
+            url: '../api/food?idRestaurant=' + btn.id + '&type=' + 3,
+            type: 'GET',
+            contentType: "application/json;charset=utf-8",
+            success:
+            function (data)
+            {
+                var elements = '';
+
+                for (var i = 0; i < data.length; i++)
+                {
+                    elements += '<button id="' + data[i][0] + '" class="restaurants" onclick="cargarRestaurante(this)"> <div> <div>' + data[i][1] + '</div> <div>' + data[i][2] + '</div> <div>' + data[i][3] + '</div> <div>' + data[i][1] + '</div> </div> </button>';
+                }
+
+                $('#listOptions').append('<div id="bebida" class="banner">BEBIDAS</div>' + elements);
             }
         }
     );
 }
 
-function regresarMenu()
+function toBack()
+{
+    $('#listOptions').empty();
+    back.style.display = "none";
+    initRestaurants();
+}
+
+function toMenu()
 {
     location.href = "dashboard.html";
-
-    
 }
