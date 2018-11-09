830e5ec9efec2c4a16af8f193e2caa7663661513
Juan Pablo Romero Londono <juan.romerol@upb.edu.co>
2018-11-09 15:06:28 -0500

    Cambios en el proyecto

--- EatNow/EatNow/src/app/app.module.ts
+++ EatNow/EatNow/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ListRestaurantsComponent } from './list-restaurants/list-restaurants.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LoginComponent,
+    DashboardComponent,
+    ListRestaurantsComponent
   ],
   imports: [
     BrowserModule,
